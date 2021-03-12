29a73f09eb0bb6fed893c31be142922a840b46d3
elbeicktalat <talatelbeick@gmail.com>
2021-03-12 13:47:58 +0100

    add copy

---
+++ lib/commands/copy.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:lamda/src/command.dart';
+
+class Copy implements Command {
+  @override
+  String get command => 'copy';
+
+  @override
+  String get description =>
+      "Copy files between directories example: copy <file name> <directory name> and you'll done.";
+
+  Future<void> copy(List<String> args) async {
+    String firstArg;
+    firstArg = args.first.toLowerCase();
+    if (firstArg == command) {
+      final file = File(args[1]);
+      final path = Directory.current.uri.toString().substring(8);
+      await file.copy('${path + args[2] + '/' + args[1]}').then((value) => print(
+          'copy \x1B[34m<${args[1]}>\x1B[0m into \x1B[32m<${args[2]}>\x1B[0m is successfully!'));
+    }
+  }
+}
--- lib/lamda.dart
+++ lib/lamda.dart
@@ -1,6 +1,7 @@
 import 'commands/make.dart';
 import 'commands/delete.dart';
 import 'commands/rename.dart';
+import 'commands/copy.dart';
 
 // This version must be updated in tandem with the pubspec version.
 const String appVersion = '1.0.0';
@@ -9,12 +10,14 @@ const String appName = 'lamda';
 Make _make = Make();
 Delete _delete = Delete();
 ReName _reName = ReName();
+Copy _copy = Copy();
 
 void run(List<String> args) {
   try {
     _make.make(args);
     _delete.delete(args);
     _reName.rename(args);
+    _copy.copy(args);
   } catch (error) {
     print(error);
   }
